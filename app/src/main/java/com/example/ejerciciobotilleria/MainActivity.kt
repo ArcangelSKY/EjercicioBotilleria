3076c0280d640659f3d03e787d233c20d62f37b3
Jota1601 <jpreyes1601@gmail.com>
2021-04-22 20:34:41 -0400

    actualizaion de data carga de imagenes en carpeta "Images"

--- app/src/main/java/com/example/ejerciciobotilleria/MainActivity.kt
+++ app/src/main/java/com/example/ejerciciobotilleria/MainActivity.kt
@@ -23,8 +23,13 @@ class MainActivity : AppCompatActivity() {
 
     fun ingresoDatos():MutableList<Botilleria>{
         var botilleria:MutableList<Botilleria> = ArrayList()
-        botilleria.add(Botilleria("Chimbombo","Vino Añiejo","Chileno","Garrafa 5 litros",2000,0))
-
+        botilleria.add(Botilleria("Santa Helena","Vino Añiejo","Chileno","Garrafa 5 litros",4750,0))
+        botilleria.add(Botilleria("Bauzá","Pisco","España","Botella 750 CC",10290,1))
+        botilleria.add(Botilleria("Chivas Regal","Whisky","España","Botella 750 CC",49990,2))
+        botilleria.add(Botilleria("Absolut Blue","Vodka","Suecia","Botella 750 CC",14290,3))
+        botilleria.add(Botilleria("Jim Beam","Vodka","Estados Unidos","Botella 700 CC",15490,4))
+        botilleria.add(Botilleria("Jose Cuervo Blanco 40°","Tequila","México","Botella 700 CC",15490,5))
+        botilleria.add(Botilleria("Barcelo Gran Añejo","Ron","España","Botella 700 CC",10290,5))
         return botilleria
     }
 }
